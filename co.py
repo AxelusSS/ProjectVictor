fe3d309dda6c530451fe7f07628b65ac5ab70453
Axelux <126026128+AxelusSS@users.noreply.github.com>
2024-03-01 11:38:23 +0100

    Delete co.py

--- co.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as pg
-import time
-time.sleep(7)
-print(pg.position())
-#pg.moveTo(x=1281, y=703, duration=2)
-time.sleep(6)
-print(pg.position())
-#pg.moveTo(x=1733, y=813, duration=2)
-time.sleep(40)
